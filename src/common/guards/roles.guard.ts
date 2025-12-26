a92b76911dc4c66c5290104a958de4247b6905ba
Tran Trong Quan <trantrongquan062@gmail.com>
2025-12-26 15:44:59 +0700

    feat: add decorators, guards, utils

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,32 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(ctx: ExecutionContext): boolean {
+    const requiredRoles =
+      this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+        ctx.getHandler(),
+        ctx.getClass(),
+      ]);
+
+    if (!requiredRoles) return true;
+
+    const req = ctx.switchToHttp().getRequest();
+    const user = req.user;
+
+    if (!user || !requiredRoles.includes(user.role)) {
+      throw new ForbiddenException('Insufficient permissions');
+    }
+
+    return true;
+  }
+}
